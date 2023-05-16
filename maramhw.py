3b14ef70cce4ee5034905def588927575659b47e
maramas1 <133688663+maramas1@users.noreply.github.com>
2023-05-16 17:46:23 +0300

    Add files via upload

---
+++ maramhw.py
@@ -0,0 +1,19 @@
+list=[1,2,3]
+list.append("a")
+print(list)
+del list[3]
+print(list)
+list.reverse()
+print(list)
+list.sort()
+print(list)
+list.insert(0,"m")
+print(list)
+
+print(list.index(3))
+people=["Ahmed","Nasser","Mohammed"]
+var=","
+print(var.join(people))
+mydic=dict(name= "maram",phonenum= 67890054321)
+print(mydic)
+Dict={}

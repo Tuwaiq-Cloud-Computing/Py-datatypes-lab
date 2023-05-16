b5b5deb98f212ebfb08a4215407ef703eb426022
maramas1 <133688663+maramas1@users.noreply.github.com>
2023-05-16 18:58:18 +0300

    Add files via upload

--- maramhw.py
+++ maramhw.py
@@ -14,6 +14,19 @@ print(list.index(3))
 people=["Ahmed","Nasser","Mohammed"]
 var=","
 print(var.join(people))
-mydic=dict(name= "maram",phonenum= 67890054321)
-print(mydic)
-Dict={}
+dict=[
+    {"name":"maram","phone":"2345667"},
+    {"name":"noha","phone":"678899"},
+    {"name":"maha","phone":"11111117"}
+]
+print(dict)
+dict.append({"name":"lila","phone":"45674"})
+print(dict)
+del dict[0]["name"]
+print(dict)
+dict[-1]["phone"]="00000"
+print(dict)
+if "name" in dict[2]:
+    print("exist")
+else:
+    print("the name dosen't exist")

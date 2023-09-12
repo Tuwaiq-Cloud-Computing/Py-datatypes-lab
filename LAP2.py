a8c8c66efe2b5f7049a7489f25e5958306c08ea2
3mor1997 <142787173+3mor1997@users.noreply.github.com>
2023-09-12 11:21:21 +0300

    Omar Alharbi

---
+++ LAP2.py
@@ -0,0 +1,35 @@
+# Make a list with 3 elements
+my_list = [1, 2, 3]
+
+# Add an element to the end of the list
+my_list.append(4)
+
+# Remove an element from the list (let's remove the second element, which is 2)
+my_list.pop(1)
+
+# Reverse the list
+my_list.reverse()
+
+# Sort the list
+my_list.sort()
+
+# Add an element at the start of the list
+my_list.insert(0, 0)
+
+
+
+people = ["Ahmed", "Nasser", "Mohammed"]
+
+# Join the elements of the list with commas and print
+formatted_list = ", ".join(people)
+print(formatted_list)
+
+# Create a list of 3 dictionaries
+people_list = [
+    {"name": "Omar", "phone_number": "123-9866-7890"},
+    {"name": "Abdulaziz", "phone_number": "123-456-3210"},
+    {"name": "Fahad", "phone_number": "123-456-7890"}
+]
+
+# Add another dictionary to the list
+new_person = {"name"…
